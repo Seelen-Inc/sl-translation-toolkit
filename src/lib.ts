1f77c796ea56f783f49c236a5e7ebdbbc3564738
eythaann <eythan.cvt@gmail.com>
2025-02-15 01:33:56 -0500

    export auto

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,7 @@
 export * from "./translators/trait.ts";
 export * from "./translators/Deepl.ts";
 export * from "./translators/Google.ts";
+export * from "./translators/Auto.ts";
 
 export * from "./implementations/trait.ts";
 export * from "./implementations/yaml.ts";
