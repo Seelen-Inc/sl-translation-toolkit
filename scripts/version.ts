e1eb7a4b3a7da8c465164038fb0b6173da4944ee
eythaann <eythan.cvt@gmail.com>
2025-02-15 16:55:43 -0500

    run linter

--- scripts/version.ts
+++ scripts/version.ts
@@ -1,31 +1,33 @@
-import denoJson from '../deno.json' with { type: 'json' };
+import denoJson from "../deno.json" with { type: "json" };
 
 const newVersion = Deno.args[0];
 if (!newVersion) {
-  console.error('Error: No version specified. Usage: deno task update-version <new-version>');
+  console.error(
+    "Error: No version specified. Usage: deno task update-version <new-version>",
+  );
   Deno.exit(1);
 }
 
 denoJson.version = newVersion;
-await Deno.writeTextFile('deno.json', JSON.stringify(denoJson, null, 2));
+await Deno.writeTextFile("deno.json", JSON.stringify(denoJson, null, 2));
 
 console.log(`Version updated to ${newVersion}`);
 
 // add to git
-await new Deno.Command('git', {
-  args: ['add', 'deno.json'],
-  stderr: 'inherit',
-  stdout: 'inherit',
+await new Deno.Command("git", {
+  args: ["add", "deno.json"],
+  stderr: "inherit",
+  stdout: "inherit",
 }).output();
 
-await new Deno.Command('git', {
-  args: ['commit', '-m', `chore(release): v${newVersion}`],
-  stderr: 'inherit',
-  stdout: 'inherit',
+await new Deno.Command("git", {
+  args: ["commit", "-m", `chore(release): v${newVersion}`],
+  stderr: "inherit",
+  stdout: "inherit",
 }).output();
 
-await new Deno.Command('git', {
-  args: ['tag', '-s', `v${newVersion}`, '-m', `v${newVersion}`],
-  stderr: 'inherit',
-  stdout: 'inherit',
+await new Deno.Command("git", {
+  args: ["tag", "-s", `v${newVersion}`, "-m", `v${newVersion}`],
+  stderr: "inherit",
+  stdout: "inherit",
 }).output();
