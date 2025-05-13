184f3d023c16a644fd94bcaf1ca8a26b9b78f3d7
eythaann <eythan.cvt@gmail.com>
2025-05-13 10:45:53 -0500

    enh: add new method

--- src/translators/trait.ts
+++ src/translators/trait.ts
@@ -1,3 +1,5 @@
+import { invokeWithGaps } from "../utils/mod.ts";
+
 export abstract class Translator<Source extends string, Target extends string> {
   // target is used to print the used API on translators that uses multiple translations APIs
   abstract name(target?: Target): string;
@@ -9,6 +11,25 @@ export abstract class Translator<Source extends string, Target extends string> {
   }
 
   abstract translate_to(to: Target, input: string): Promise<string>;
+
+  async translate_to_multiple(
+    to: Target[],
+    input: string,
+  ): Promise<Record<Target, string>> {
+    const translated = {} as Record<Target, string>;
+
+    const tasks = to.map((target) => async () => {
+      translated[target] = await this.translate_to(target, input);
+    });
+
+    console.info(
+      `  - translating 1 text to ${tasks.length} targets with a gap of 200ms`,
+    );
+    const result = await invokeWithGaps(tasks, 200);
+    await Promise.all(result);
+
+    return translated;
+  }
 }
 
 export type UnspecifiedTranslator = Translator<string, string>;
